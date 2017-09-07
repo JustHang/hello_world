bde8f56dffebbb86aa610723f7189102dcc79be3
liuhang10 <Just_Hang@126.com>
2017-09-08 00:29:50 +0800

    Git测试第二次

--- hello_world_module/src/com/jd/demo/Helloworld.java
+++ hello_world_module/src/com/jd/demo/Helloworld.java
@@ -2,6 +2,6 @@ package com.jd.demo;
 
 public class Helloworld {
     public static void main(String[] args){
-        System.out.print("你好,世界,加油!再加油!");
+        System.out.print("你好,世界,加油!再加油!好哒!");
     }
 }
---
+++ hello_world_module/src/com/jd/demo/TestGit.java
@@ -0,0 +1,4 @@
+package com.jd.demo;
+
+public class TestGit {
+}
